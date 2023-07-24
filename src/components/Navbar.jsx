f533f2d72331279786e587fc086edbfb2eb57b96
Your Name <you@example.com>
2023-07-25 00:22:15 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import {stack} from '@mui/material';
+import {Link} from 'react-router-dom';
+import {logo} from '../utils/constants';
+import SearchBar from './SearchBar';
+const Navbar = () => (
+  <stack direction="row" alignItems="centre" p={2}
+  sx={{position:'sticky',background:'#000',top:0,justifyContent:'space-between'}}>
+
+<Link to="/" style={{display:'flex',alignItems:'center'}}>
+  <img src={logo} alt="logo" height={45}/>
+</Link>
+<SearchBar />
+</stack>
+)
+export default Navbar
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import {UseState} from 'react';
+import { UseNavigate}  from 'react-router-dom';
+import {Paper,IconButton} from '@mui/material';
+import {SearchIcon} from '@mui/icons-material';
+
+const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
